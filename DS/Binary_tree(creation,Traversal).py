61e908ea285619ca2c8b7e46fc9aa1b41054d0c0
sagnik938 <70232923+sagnik938@users.noreply.github.com>
2022-01-10 10:42:38 +0530

    Update Binary_tree(creation,Traversal).py

--- DS/Binary_tree(creation,Traversal).py
+++ DS/Binary_tree(creation,Traversal).py
@@ -4,8 +4,6 @@ class Node:
         self.left = None
         self.right = None
 
-arr = []
-
 def CreateTree():
     temp = Node()
     val = input("Enter Value of Node, N for no node")
@@ -21,37 +19,57 @@ def CreateTree():
 
     return(temp)
 
-def preorder(root):
+def preorder(root,arr):
     if(root == None):
         return
 
     arr.append(root.data)
-    preorder(root.left)
-    preorder(root.right)
+    preorder(root.left,arr)
+    preorder(root.right,arr)
 
-def inorder(root):
+def inorder(root,arr):
     if(root == None):
         return
 
-    inorder(root.left)
-    arr.append(root.data)
-    inorder(root.right)
+    inorder(root.left,arr)
+    arr.append( root.data )
+    inorder(root.right,arr)
 
-def postorder(root):
+def postorder(root,arr):
     if(root == None):
         return
 
-    postorder(root.left)
-    postorder(root.right)
+    postorder(root.left,arr)
+    postorder(root.right,arr)
     arr.append(root.data)
+
+def traversal(root):
+    opt = 0
+    arr = []
+    while(opt != 4):
+        opt = int(input("Enter\n<1> for inorder\n<2> for post order\n<3> for preorder\n<4> to exit\n"))
+        if( opt == 1):
+           arr = [] 
+           inorder(root,arr)
+           print( arr ) 
+        if( opt == 2):
+           arr = []
+           postorder(root,arr) 
+           print( arr )
+        if( opt == 3):
+           arr = []
+           preorder(root,arr) 
+           print( arr )
+
+
+
+
 
 root = CreateTree()  
-preorder(root)
-print(arr)
-inorder(root)
-print(arr)
-postorder(root)
-print(arr)
+traversal(root)
+
+
+
 
 
 
