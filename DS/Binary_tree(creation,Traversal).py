6c47da108545d949244265d4f53eecbd9afd16e3
sagnik938 <70232923+sagnik938@users.noreply.github.com>
2022-01-11 11:38:14 +0530

    Update Binary_tree(creation,Traversal).py

--- DS/Binary_tree(creation,Traversal).py
+++ DS/Binary_tree(creation,Traversal).py
@@ -43,11 +43,45 @@ def postorder(root,arr):
     postorder(root.right,arr)
     arr.append(root.data)
 
-def traversal(root):
+def height(node):
+    if node is None:
+        return -1 ; 
+
+    else :
+
+        # Compute the depth of each subtree
+        lDepth = height(node.left)
+        rDepth = height(node.right)
+
+        # Use the larger one
+        if (lDepth > rDepth):
+            return lDepth+1
+        else:
+            return rDepth+1
+        
+def dolvlOrder(root,level,arr):
+    if( root == None):
+        return(0)
+    if( level == 1):
+        arr.append(root.data)
+    else:
+        dolvlOrder(root.left,level-1,arr)
+        dolvlOrder(root.right,level-1,arr)
+     
+            
+def levelOrder(root,arr):
+    #Write your code here
+    ht = height(root)
+    i = 1
+    while(i<=ht+1):
+        dolvlOrder(root,i,arr)
+        i += 1
+
+def operations(root):
     opt = 0
     arr = []
-    while(opt != 4):
-        opt = int(input("Enter\n<1> for inorder\n<2> for post order\n<3> for preorder\n<4> to exit\n"))
+    while(opt != 6):
+        opt = int(input("Enter\n<1> for inorder\n<2> for post order\n<3> for preorder\n<4> to print height\n<5> to do level order traversal\n<6> to exit"))
         if( opt == 1):
            arr = [] 
            inorder(root,arr)
@@ -60,15 +94,16 @@ def traversal(root):
            arr = []
            preorder(root,arr) 
            print( arr )
-
-
-
+        if(opt == 4):
+            print(height(root))
+        if(opt == 5):
+            arr = []
+            levelOrder(root,arr) 
+            print( arr )
 
 
 root = CreateTree()  
-traversal(root)
-
-
+operations(root)
 
 
 
